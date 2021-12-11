ea1112f6c745aeecbfb859c104cb50736cb8ddc5
sharg2016 <85231278+sharg2016@users.noreply.github.com>
2021-12-12 00:15:47 +0530

    Add files via upload

---
+++ TO_UPPER.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+char to_upper(c);
+
+void main()
+{
+ch
+clrscr();
+scanf("%c",&c);
+w=to_upper(c);
+printf("%c",w);
+getch();
+}
+
+char to_upper (char c){
+	int d,e;
+	d=("%d",c);
+	d=d-32;
+	e=("%c",d);
+	return e;
+}
