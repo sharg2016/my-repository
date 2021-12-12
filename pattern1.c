3fe7ebbb384a850225f315dffb102998cb48182b
sharg2016 <85231278+sharg2016@users.noreply.github.com>
2021-12-12 15:31:54 +0530

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+	  {
+	  int n,i,j;
+	  clrscr();
+	  printf("Enter your number = ");
+	  scanf("%d",&n);
+	  for(i=1;i<=n;i++)
+	  {
+	   for(j=1;j<=i;j++)
+	   {
+	    printf("*");
+	   }
+	   printf("\n");
+	   }
+return(0);
+}
+                                                                 *
+                                                                 **
+                                                                 ***
+                                                                 ****
+                                                                 *****
